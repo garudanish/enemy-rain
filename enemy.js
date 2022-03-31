5c81dbbf54cecf338d20915669e48fa0faa6396a
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-03-31 20:06:00 +0900

    적 죽이기, 게임 종료 기능 구현

--- enemy.js
+++ enemy.js
@@ -2,21 +2,53 @@ class Enemy {
   constructor(game) {
     this.game = game;
     this.enemy = document.createElement("div");
-    this.enemy.className = "enemy-div";
-    this.enemy.style.left = Math.floor(Math.random() * (800 - 45)) + "px";
-    bg.appendChild(this.enemy);
-
     this.enemy.top = 0;
+    this.enemy.left = Math.floor(
+      Math.random() * (bg.getBoundingClientRect().width - 45)
+    );
+    this.moveIntervalId = null;
+    this.drawEnemy();
+    this.moveEnemy();
+  }
+
+  drawEnemy() {
+    this.enemy.className = "enemy-div";
     this.enemy.style.top = this.enemy.top + "px";
+    this.enemy.style.left = this.enemy.left + "px";
+    bg.appendChild(this.enemy);
+  }
+
+  moveEnemy() {
     this.moveIntervalId = setInterval(() => {
       this.moveDown();
     }, 50);
   }
 
   moveDown() {
-    this.enemy.top += 1;
+    if (
+      this.enemy.top + this.enemy.offsetHeight >
+      this.game.hero.hero.offsetTop
+    ) {
+      // y축 범위 안에 들어왔을 때
+
+      const isInLeftBoundary =
+        this.enemy.offsetLeft + this.enemy.offsetWidth >
+        this.game.hero.hero.offsetLeft;
+      const isInRightBoundary =
+        this.enemy.offsetLeft <
+        this.game.hero.hero.offsetLeft + this.game.hero.hero.offsetWidth;
+      if (isInLeftBoundary && isInRightBoundary) {
+        this.enemy.classList.add("dead");
+        clearInterval(this.moveIntervalId);
+        setTimeout(() => {
+          this.enemy.remove();
+        }, 800);
+      }
+    }
+
+    this.enemy.top += 5;
     this.enemy.style.top = this.enemy.top + "px";
-    if (this.enemy.style.top === `${600 - 54}px`) {
+    if (this.enemy.top > 600 - 54) {
       this.game.quit();
     }
   }
--- init.js
+++ init.js
@@ -1,33 +1,35 @@
 class Game {
   constructor() {
     this.hero = null;
-    this.enemy = [];
+    this.enemies = [];
     this.startGame();
   }
 
   startGame() {
-    window.addEventListener("keydown", (e) => {
-      if (e.key === "ArrowLeft") {
-        this.hero.moveLeft();
-      } else if (e.key === "ArrowRight") {
-        this.hero.moveRight();
-      }
-    });
-
     this.hero = new Hero(this);
+    window.addEventListener("keydown", this.onKeydown);
     this.generateEnemy();
   }
 
+  onKeydown = (e) => {
+    if (e.key === "ArrowLeft") {
+      this.hero.moveLeft();
+    } else if (e.key === "ArrowRight") {
+      this.hero.moveRight();
+    }
+  };
+
   generateEnemy() {
     this.generateIntervalId = setInterval(() => {
-      this.enemy.push(new Enemy(this));
+      this.enemies.push(new Enemy(this));
     }, 3000);
   }
 
   quit() {
     clearInterval(this.generateIntervalId);
     this.hero.hero.remove();
-    this.enemy.map((enemy) => {
+    window.removeEventListener("keydown", this.onKeydown);
+    this.enemies.map((enemy) => {
       enemy.enemy.remove();
       clearInterval(enemy.moveIntervalId);
     });
